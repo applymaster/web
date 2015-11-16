2f063ea7cd4e7d4cc4e4853ac702a53511c55c20
udall <xmudall@gmail.com>
2015-11-16 20:59:09 +0800

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,93 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name webApp
+ * @description
+ * # webApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('webApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      /*.when('/', {
+        templateUrl: '../views/main_bak.html',
+        controller: 'MainBakCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })*/
+      .when('/', {
+        templateUrl: '../views/main.html',
+        controller: 'HomeCtrl'
+      })
+      .when('/login', {
+        templateUrl: '../views/user/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/register/select', {
+        templateUrl: '../views/user/register_select.html',
+      })
+      .when('/register/applicant', {
+        templateUrl: '../views/user/register_applicant.html',
+        controller: 'RegisterAppCtrl'
+      })
+      .when('/register/applicant_more', {
+        templateUrl: '../views/user/register_applicant_more.html',
+        controller: 'ApplicantInfoCtrl'
+      })
+      .when('/register/applicant_compl', {
+        templateUrl: '../views/user/register_applicant_compl.html',
+        controller: 'RegisterAppComplCtrl'
+      })
+      .when('/register/teacher', {
+        templateUrl: '../views/user/register_teacher.html',
+        controller: 'RegisterTeaCtrl'
+      })
+      .when('/register/teacher_more', {
+        templateUrl: '../views/user/register_teacher_more.html',
+        controller: 'TeacherInfoCtrl'
+      })
+      .when('/register/teacher_compl', {
+        templateUrl: '../views/user/register_teacher_compl.html',
+        controller: 'RegisterTeaComplCtrl'
+      })
+      .when('/register/active', {
+        templateUrl: '../views/user/register_active.html',
+        controller: 'ActiveCtrl'
+      })
+      .when('/register/verify_email/:key', {
+        templateUrl: 'views/loading.html',
+        controller: 'VerifyEmailCtrl'
+      })
+      .when('/order', {
+        templateUrl: 'views/order_list.html',
+        controller: 'OrderListCtrl'
+      })
+      .when('/order/:id', {
+        templateUrl: 'views/order_detail.html',
+        controller: 'OrderDetailCtrl'
+      })
+      .when('/account', {
+        redirectTo: 'account/overview'
+      })
+      .when('/account/overview', {
+        templateUrl: 'views/account/overview.html',
+        controller: 'AccountOverviewCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ app/scripts/user/controllers.js
@@ -0,0 +1,238 @@
+'use strict';
+
+var app = angular.module('webApp');
+
+app.controller('LoginCtrl', ['$scope', 'Login', '$location', '$cookies',
+  function($scope, Login, $location, $cookies) {
+    $scope.loginError = '';
+
+    $scope.login = function(email, password) {
+
+      if (!email || email === '' || !password || password === '') {
+        $scope.loginError = 'Email or password cannot be null!';
+        return;
+      }
+      var pattern = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+      if (!pattern.test(email)) {
+        $scope.loginError = 'Wrong Email format!';
+        return;
+      }
+
+      Login.save(
+        {email:email, password:md5(password, email)},
+        function(response) {
+          if (response.success) {
+            $cookies.putObject("user", response.user);
+            $scope.setUser(response.user);
+            $location.path('/home');
+          } else {
+            $scope.loginError = response.msg;
+          }
+        },
+        function(response) {
+          $scope.loginError = response.data;
+        }
+      );
+    }
+  }
+]);
+
+app.controller('RegisterAppCtrl', ['$scope', '$http', 'Register', 'Login', '$location',
+  function($scope, $http, Register, Login, $location) {
+    $scope.loginError = '';
+
+    $scope.register = function(email, password, passwordConfirm) {
+      if (!$scope.assignment)
+      {
+        $scope.loginError = 'You need to agree with our policy fo continue!';
+        return;
+      }
+
+      if (!email || email === '' || !password || password === '' || !passwordConfirm || passwordConfirm === '') {
+        $scope.loginError = 'Email or password cannot be null!';
+        return;
+      }
+      if (password != passwordConfirm) {
+        $scope.loginError = 'Two passwords are not the same!';
+        return;
+      }
+      var pattern = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+      if (!pattern.test(email)) {
+        $scope.loginError = 'Wrong Email format!';
+        return;
+      }
+
+      Register.save(
+        {email:email, password:md5(password, email), type:0},
+        function(response) {
+          if (response.success) {
+            Login.save(
+              {email:email, password:md5(password, email)},
+              function(response) {
+                if (response.success) {
+                  //$location.path('/home');
+                  $location.path('/register/applicant_more');
+                } else {
+                  $scope.loginError = response.msg;
+                }
+              },
+              function(response) {
+                $scope.loginError = response.data;
+              }
+            );
+          } else {
+            $scope.loginError = response.msg;
+          }
+        },
+        function(response) {
+          $scope.loginError = response.data;
+        }
+      );
+    }
+  }
+]);
+
+app.controller('ApplicantInfoCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $scope.educations = [{name:"",id:0}];
+    $scope.addEducation = function() {
+      $scope.educations.push({name:"",id:0});
+    };
+
+    $('#datepicker').datepicker({
+      format: 'yyyy-mm-dd',
+      language: 'zh-CN'
+    });
+
+    $scope.submit = function() {
+      // save user info
+
+      // jump to active page
+      $scope.navTo('register/active');
+    };
+  }
+]);
+
+app.controller('RegisterAppComplCtrl', ['$scope', '$http',
+  function($scope, $http) {
+  }
+]);
+
+app.controller('RegisterTeaCtrl', ['$scope', '$http', 'Register', 'Login', '$location',
+  function($scope, $http, Register, Login, $location) {
+    $scope.setBreadcrumb([
+      { href: "#/home", name: "申请大师" },
+      { href: "#/register/teacher", name: "注册留学顾问" }
+    ]);
+    $scope.loginError = '';
+
+    $scope.register = function(email, password, passwordConfirm) {
+      if (!$scope.assignment)
+      {
+        $scope.loginError = 'You need to agree with our policy fo continue!';
+        return;
+      }
+
+      if (!email || email === '' || !password || password === '' || !passwordConfirm || passwordConfirm === '') {
+        $scope.loginError = 'Email or password cannot be null!';
+        return;
+      }
+      if (password != passwordConfirm) {
+        $scope.loginError = 'Two passwords are not the same!';
+        return;
+      }
+      var pattern = /^\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+      if (!pattern.test(email)) {
+        $scope.loginError = 'Wrong Email format!';
+        return;
+      }
+
+      Register.save(
+        {email:email, password:md5(password, email), type:1},
+        function(response) {
+          if (response.success) {
+            Login.save(
+              {email:email, password:md5(password, email)},
+              function(response) {
+                if (response.success) {
+                  //$location.path('/home');
+                  $location.path('/register/teacher_more');
+                } else {
+                  $scope.loginError = response.msg;
+                }
+              },
+              function(response) {
+                $scope.loginError = response.data;
+              }
+            );
+          } else {
+            $scope.loginError = response.msg;
+          }
+        },
+        function(response) {
+          $scope.loginError = response.data;
+        }
+      );
+    }
+  }
+]);
+
+app.controller('TeacherInfoCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    $scope.setBreadcrumb([
+      { href: "#/home", name: "申请大师" },
+      { href: "javascript:void()", name: "顾问详细信息" }
+    ]);
+    $scope.educations = [{name:"",id:0}];
+    $scope.addEducation = function() {
+      $scope.educations.push({name:"",id:0});
+    };
+
+    $('#datepicker').datepicker({
+      format: 'yyyy-mm-dd',
+      language: 'zh-CN'
+    });
+
+    $scope.submit = function() {
+      // save user info
+
+      // jump to active page
+      $scope.navTo('register/active');
+    }
+  }
+]);
+
+app.controller('RegisterTeaComplCtrl', ['$scope', '$http',
+  function($scope, $http) {
+  }
+]);
+
+app.controller('ActiveCtrl', ['$scope', '$http',
+  function($scope, $http) {
+    // send verify email
+    $http.get('/verifyEmail').
+      success(function(data) {
+        console.log('email sent.' + JSON.stringify(data));
+      }).
+      error(function(data, status, headers, config) {
+        console.log('send email failed.' + JSON.stringify(data));
+      });
+
+    $scope.jumpToMailServer = function() {
+
+    }
+  }
+]);
+
+app.controller('VerifyEmailCtrl', ['$scope', '$http', '$routeParams',
+  function($scope, $http, $routeParams) {
+    var key = $routeParams.key;
+    $http.get('/verifyEmailCallback/' + key).
+      success(function() {
+        $scope.navTo('register/applicant_compl');
+      }).
+      error(function(data) {
+        $scope.noti = "验证失败，请重新验证";
+      });
+  }
+]);
